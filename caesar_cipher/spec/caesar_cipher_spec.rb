62d7bbe88848a4395285a96231599de1761cdc4f
Peter Abah <peterabah.ob@gmail.com>
2021-05-18 19:06:37 +0100

    add more tests

--- caesar_cipher/spec/caesar_cipher_spec.rb
+++ caesar_cipher/spec/caesar_cipher_spec.rb
@@ -6,4 +6,28 @@ describe '#caesar_cipher' do
     result = 'bcdef'
     expect(caesar_cipher(word, 1)).to eql(result)
   end
+
+  it 'shifts an uppercase word by a number' do
+    word = 'STUFFS'
+    result = 'XYZKKX'
+    expect(caesar_cipher(word, 5)).to eql(result)
+  end
+
+  it 'shifts a mixed case word by a number' do
+    word = 'aBCDefGh'
+    result = 'fGHIjkLm'
+    expect(caesar_cipher(word, 5)).to eql(result)
+  end
+
+  it 'shifts back to the beginning if number is large' do
+    word = 'aBCDefGh'
+    result = 'aBCDefGh'
+    expect(caesar_cipher(word, 26)).to eql(result)
+  end
+
+  it 'shifts a word with extremely large numbers' do
+    word = 'aBCDefGh'
+    result = 'aBCDefGh'
+    expect(caesar_cipher(word, 260)).to eql(result)
+  end
 end
